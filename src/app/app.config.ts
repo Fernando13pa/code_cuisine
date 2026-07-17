0ad06cd7bde9094447229d41e636348f21a18efe
Fernando Cun Ramirez <fernando13pa@hotmail.com>
2026-07-17 19:39:52 +0200

    fixen end version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    // Hash routing avoids needing server-side rewrite rules on static FTP hosting.
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
   ]
 };
